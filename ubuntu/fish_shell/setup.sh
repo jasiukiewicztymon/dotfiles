af8c187e60d761fb6f92ce788c82cb0bf321ab5e
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-05-12 23:46:20 +0200

    Update setup.sh

--- ubuntu/fish_shell/setup.sh
+++ ubuntu/fish_shell/setup.sh
@@ -1,10 +1,18 @@
 sudo apt update
-sudo apt install fish
+sudo apt install fish -y
 fish
+
 sudo apt update
-sudo apt install vim
+sudo apt install vim -y
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 echo -e "if status is-interactive\n\toh-my-posh init fish | source\noh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/atomic.omp.json' | source\nend" > ~/.config/fish/config.fish
 exec fish
 . ~/.config/fish/config.fish
+
+sudo apt install build-essential -y
+sudo apt-get install manpages-dev -y
+
+sudo apt install python3.8 -y
+
+sudo apt install nodejs -y
